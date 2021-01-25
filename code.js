b50d2cc2447d03ea9ba14f83ec590b5ac03cb091
Vaibhav-jain-2008 <76032294+Vaibhav-jain-2008@users.noreply.github.com>
2021-01-25 12:55:14 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,31 @@
+
+onEvent("Sa", "click", function( ) {
+  playSound("assets/SA.mp3", false);
+});
+onEvent("Re", "click", function( ) {
+  playSound("assets/Ra.mp3", false);
+});
+onEvent("bga", "click", function( ) {
+  playSound("assets/bGa.mp3", false);
+});
+onEvent("Ga", "click", function( ) {
+  playSound("assets/bGa.mp3", false);
+});
+onEvent("Ma", "click", function( ) {
+  playSound("assets/Ma.mp3", false);
+});
+onEvent("Pa", "click", function( ) {
+  playSound("assets/Pa.mp3", false);
+});
+onEvent("Dha", "click", function( ) {
+  playSound("assets/Dha.mp3", false);
+});
+onEvent("bne", "click", function( ) {
+  playSound("assets/bna.mp3", false);
+});
+onEvent("Ne", "click", function( ) {
+  playSound("assets/ne.mp3", false);
+});
+onEvent("lSa", "click", function( ) {
+  playSound("assets/llsa.mp3", false);
+});
